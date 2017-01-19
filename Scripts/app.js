3d5d288aa2a84b29dc5b11aa3d402610594035be
sekcheong <scheong@gmail.com>
2017-01-18 19:12:59 -0600

    more changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,68 +3,157 @@ var Test;
     var Application = (function () {
         function Application() {
         }
-        Application.prototype.run = function () {
-            var l = 1024;
-            var rect = d3.select("body")
-                .append("svg")
-                .attr("width", l)
-                .attr("height", l)
-                .append("rect")
-                .attr("id", "main")
+        Application.prototype.drawGrid = function (g, lines) {
+            var c = "#b0b0b0";
+            g.append("rect")
                 .attr("x", 0)
                 .attr("y", 0)
-                .attr("width", l)
+                .attr("width", "1024")
                 .attr("fill", "none")
-                .attr("stroke-width", 1)
-                .attr("stroke", "#606060")
-                .attr("height", l);
-            var r = d3.select("svg");
-            r.append("line")
+                .attr("stroke-width", 2)
+                .attr("stroke", c)
+                .attr("height", "1024");
+            if (lines) {
+                for (var i = 0; i < 64; i++) {
+                    g.append("line")
+                        .attr("x1", 0)
+                        .attr("y1", i * 16)
+                        .attr("x2", 1024)
+                        .attr("y2", i * 16)
+                        .attr("stroke-width", 1)
+                        .attr("stroke", "#008000");
+                    g.append("line")
+                        .attr("x1", i * 16)
+                        .attr("y1", 0)
+                        .attr("x2", i * 16)
+                        .attr("y2", 1024)
+                        .attr("stroke-width", 1)
+                        .attr("stroke", "#008000");
+                }
+            }
+            g.append("line")
                 .attr("x1", 0)
                 .attr("y1", 0)
-                .attr("x2", l)
-                .attr("y2", l)
-                .attr("stroke-width", 1)
-                .attr("stroke", "#606060");
-            var s = d3.select("svg");
-            s.append("line")
-                .attr("x1", l)
-                .attr("y1", 0)
-                .attr("x2", 0)
-                .attr("y2", l)
-                .attr("stroke-width", 1)
-                .attr("stroke", "#606060");
-            var u = d3.select("svg");
-            u.append("line")
-                .attr("x1", l / 2)
-                .attr("y1", 0)
-                .attr("x2", l / 2)
-                .attr("y2", l)
-                .attr("stroke-width", 1)
-                .attr("stroke", "#606060");
-            var v = d3.select("svg");
-            v.append("line")
+                .attr("x2", 1024)
+                .attr("y2", 1024)
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("line")
                 .attr("x1", 0)
-                .attr("y1", l / 2)
-                .attr("x2", l)
-                .attr("y2", l / 2)
-                .attr("stroke-width", 1)
-                .attr("stroke", "#606060");
-            //var v = d3.select("svg");
-            //r.append("line")
-            //	.attr("x1", 1024)
-            //	.attr("y1", 0)
-            //	.attr("x2", 0)
-            //	.attr("y2", 1024)
-            //	.attr("stroke-width", 1)
-            //	.attr("stroke", "#606060");
-            r.append("circle")
-                .attr("cx", l / 2)
-                .attr("cy", l / 2)
-                .attr("r", (l / 2) - (l / 16))
+                .attr("y1", 1024)
+                .attr("x2", 1024)
+                .attr("y2", 0)
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("line")
+                .attr("x1", 512)
+                .attr("y1", 0)
+                .attr("x2", 512)
+                .attr("y2", 1024)
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("line")
+                .attr("x1", 0)
+                .attr("y1", 512)
+                .attr("x2", 1024)
+                .attr("y2", 512)
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("rect")
+                .attr("x", 64)
+                .attr("y", 64)
+                .attr("width", 1024 - 128)
+                .attr("height", 1024 - 128)
                 .attr("fill", "none")
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("circle")
+                .attr("cx", 512)
+                .attr("cy", 512)
+                .attr("r", 448)
+                .attr("fill", "none")
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("circle")
+                .attr("cx", 512)
+                .attr("cy", 512)
+                .attr("r", 272)
+                .attr("fill", "none")
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("circle")
+                .attr("cx", 512)
+                .attr("cy", 512)
+                .attr("r", 192)
+                .attr("fill", "none")
+                .attr("stroke-width", 2)
+                .attr("stroke", c);
+            g.append("line")
+                .attr("x1", 0)
+                .attr("y1", 320)
+                .attr("x2", 1024)
+                .attr("y2", 320)
                 .attr("stroke-width", 1)
-                .attr("stroke", "#606060");
+                .attr("stroke", c);
+            g.append("line")
+                .attr("x1", 0)
+                .attr("y1", 704)
+                .attr("x2", 1024)
+                .attr("y2", 704)
+                .attr("stroke-width", 1)
+                .attr("stroke", c);
+            g.append("line")
+                .attr("x1", 320)
+                .attr("y1", 0)
+                .attr("x2", 320)
+                .attr("y2", 1024)
+                .attr("stroke-width", 1)
+                .attr("stroke", c);
+            g.append("line")
+                .attr("x1", 704)
+                .attr("y1", 0)
+                .attr("x2", 704)
+                .attr("y2", 1024)
+                .attr("stroke-width", 1)
+                .attr("stroke", c);
+        };
+        Application.prototype.run = function () {
+            var c = "#808080";
+            //var img = d3.select("body")
+            //	.append("img")
+            //	.attr("width", 100)
+            //	.attr("height", 100);
+            var svg = d3.select("body")
+                .append("svg")
+                .attr("width", "512")
+                .attr("height", "512")
+                .attr("viewBox", "0 0 1024 1024");
+            //var g = svg;
+            var g = d3.select("svg").append("g");
+            //g.attr("transform", "translate(128,128) scale(" +(1024/1024) + ")");
+            this.drawGrid(g);
+            g.append("rect")
+                .attr("x", 96)
+                .attr("y", 96)
+                .attr("width", 832)
+                .attr("height", 832)
+                .attr("fill", "none")
+                .attr("stroke-width", 64)
+                .attr("stroke", "#000000");
+            g.append("line")
+                .attr("x1", 224)
+                .attr("y1", 512)
+                .attr("x2", 480)
+                .attr("y2", 768)
+                .attr("stroke-width", 10 * 16)
+                .attr("stroke", "#000000");
+            g.append("line")
+                .attr("x1", 368)
+                .attr("y1", 768)
+                .attr("x2", 368 + 27 * 16)
+                .attr("y2", 768 - 27 * 16)
+                .attr("stroke-width", 10 * 16)
+                .attr("stroke", "#000000");
             console.log("running");
         };
         return Application;
--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,33 +1,45 @@
 ﻿namespace Test {
 	declare var d3;
 	export class Application {
-		public run() {
-			var c = "#000000";
 
-			var svg = d3.select("body")
-				.append("svg")
-				.attr("width", 2048)
-				.attr("height", 2048);
-
-			var g = d3.select("svg").append("g");
-			g.attr("transform", "translate(30,30) scale(0.03125)");
-
+		private drawGrid(g, lines?) {
+			var c = "#b0b0b0";
 			g.append("rect")
 				.attr("x", 0)
 				.attr("y", 0)
 				.attr("width", "1024")
 				.attr("fill", "none")
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c)
 				.attr("height", "1024");
 
+			if (lines) {
+				for (let i = 0; i < 64; i++) {
+					g.append("line")
+						.attr("x1", 0)
+						.attr("y1", i * 16)
+						.attr("x2", 1024)
+						.attr("y2", i * 16)
+						.attr("stroke-width", 1)
+						.attr("stroke", "#008000");
+
+					g.append("line")
+						.attr("x1", i * 16)
+						.attr("y1", 0)
+						.attr("x2", i * 16)
+						.attr("y2", 1024)
+						.attr("stroke-width", 1)
+						.attr("stroke", "#008000");
+
+				}
+			}
 
 			g.append("line")
 				.attr("x1", 0)
 				.attr("y1", 0)
 				.attr("x2", 1024)
 				.attr("y2", 1024)
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 
@@ -36,7 +48,7 @@
 				.attr("y1", 1024)
 				.attr("x2", 1024)
 				.attr("y2", 0)
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 
@@ -45,7 +57,7 @@
 				.attr("y1", 0)
 				.attr("x2", 512)
 				.attr("y2", 1024)
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 
@@ -54,17 +66,17 @@
 				.attr("y1", 512)
 				.attr("x2", 1024)
 				.attr("y2", 512)
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 
 			g.append("rect")
 				.attr("x", 64)
 				.attr("y", 64)
-				.attr("width", 1024-128)
-				.attr("height", 1024-128)
+				.attr("width", 1024 - 128)
+				.attr("height", 1024 - 128)
 				.attr("fill", "none")
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 
@@ -74,15 +86,15 @@
 				.attr("cy", 512)
 				.attr("r", 448)
 				.attr("fill", "none")
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 			g.append("circle")
 				.attr("cx", 512)
 				.attr("cy", 512)
-				.attr("r", 276.88)
+				.attr("r", 272)
 				.attr("fill", "none")
-				.attr("stroke-width", 1)
+				.attr("stroke-width",2)
 				.attr("stroke", c);
 
 			g.append("circle")
@@ -90,44 +102,89 @@
 				.attr("cy", 512)
 				.attr("r", 192)
 				.attr("fill", "none")
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 2)
 				.attr("stroke", c);
 
 
 			g.append("line")
 				.attr("x1", 0)
-				.attr("y1", 316.25)
+				.attr("y1", 320)
 				.attr("x2", 1024)
-				.attr("y2", 316.25)
+				.attr("y2", 320)
 				.attr("stroke-width", 1)
 				.attr("stroke", c);
 
 			g.append("line")
 				.attr("x1", 0)
-				.attr("y1", 707.75)
+				.attr("y1", 704)
 				.attr("x2", 1024)
-				.attr("y2", 707.75)  
+				.attr("y2", 704)
 				.attr("stroke-width", 1)
 				.attr("stroke", c);
 
 
 			g.append("line")
-				.attr("x1", 316.25)
+				.attr("x1", 320)
 				.attr("y1", 0)
-				.attr("x2", 316.25)
+				.attr("x2", 320)
 				.attr("y2", 1024)
 				.attr("stroke-width", 1)
 				.attr("stroke", c);
 
 			g.append("line")
-				.attr("x1", 707.75)
+				.attr("x1", 704)
 				.attr("y1", 0)
-				.attr("x2", 707.75)
+				.attr("x2", 704)
 				.attr("y2", 1024)
 				.attr("stroke-width", 1)
 				.attr("stroke", c);
+		}
+
+		public run() {
+			var c = "#808080";
+
+			//var img = d3.select("body")
+			//	.append("img")
+			//	.attr("width", 100)
+			//	.attr("height", 100);
 
 
+			var svg = d3.select("body")
+				.append("svg")
+				.attr("width", "512")
+				.attr("height", "512")
+				.attr("viewBox", "0 0 1024 1024");
+			//var g = svg;
+			var g = d3.select("svg").append("g");
+			//g.attr("transform", "translate(128,128) scale(" +(1024/1024) + ")");
+
+			this.drawGrid(g);
+
+			g.append("rect")
+				.attr("x", 96)
+				.attr("y", 96)
+				.attr("width", 832)
+				.attr("height", 832)
+				.attr("fill", "none")
+				.attr("stroke-width", 64)
+				.attr("stroke", "#000000");
+				
+			g.append("line")
+				.attr("x1", 224)
+				.attr("y1", 512)
+				.attr("x2", 480)
+				.attr("y2", 768)
+				.attr("stroke-width", 10*16)
+				.attr("stroke", "#000000");
+		
+
+			g.append("line")				
+				.attr("x1", 368)
+				.attr("y1", 768)
+				.attr("x2", 368+27*16)
+				.attr("y2", 768-27*16)
+				.attr("stroke-width", 10*16)
+				.attr("stroke", "#000000");
 			console.log("running");
 		}
 	}
